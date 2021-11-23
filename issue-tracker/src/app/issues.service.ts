6e0e635fc621050db74dd006e710e3c6f282499e
Chuks Akibot <juniorakibor@yahoo.com>
2021-11-23 12:16:05 +0100

    mock-issues created and get pending issues function created

--- issue-tracker/src/app/issues.service.ts
+++ issue-tracker/src/app/issues.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Issues } from './issues';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IssuesService {
+  // issue property to hold our issue data 
 
+  private issues: Issues[] = [];
   constructor() { }
+
+  // return all issues that have not been completed
+  getPendingIssues(): Issues[] {
+    return this.issues.filter(issue => !issue.completed)
+  }
 }
